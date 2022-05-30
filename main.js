2330d465ef141769df1caa43202893e40b64ade1
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-30 16:55:16 +0100

    Count of positives / sum of negatives

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 console.clear();
 
-const summation = (num) => {
-  let count = 0;
-  for (let i = 1; i <= num; i++) count += i;
+const numbers = [
+  1, 2, 3, 4, 5, 6, 10, 7, -1, -7, -3, -8, -9, -4,
+];
 
-  return count;
+const countPositivesSumNegatives = (input) => {
+  if (input.length < 1) return [];
+  let newArray = [0, 0];
+  input.forEach((item) => {
+    if (item > 0) return (newArray[0] += 1);
+    else {
+      newArray[1] += item;
+    }
+  });
+  return newArray;
 };
 
-console.log(summation(4));
+console.log(countPositivesSumNegatives(numbers));
