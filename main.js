b579ca0525b6e8cba2a77fdad80ca36a287f8e3e
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-06-02 21:09:02 +0100

    Square of (n) Sum

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.clear();
 
-// Sum of Arrays
+// Square of (n) Sum
 
-const arrays = [1, 5.2, 4, 0, -1];
+const arrays = [1, 2, 2, 3];
 console.log(sum(arrays));
 
 function sum(input) {
-  return input.reduce((a, b) => a + b, 0);
+  return input.reduce((acc, curr) => acc + curr ** 2, 0);
 }
