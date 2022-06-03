7160eab04e307fd21b68db2478dc0c57c284dd05
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-06-03 19:39:38 +0100

    Ternary method

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 console.clear();
 
 const areaOrPerimeter = (l, w) => {
-  if (l === w) return l * w;
-  else {
-    return 2 * (l + w);
-  }
+  return l === w ? l * w : 2 * (l + w);
 };
 console.log(areaOrPerimeter(6, 10));
