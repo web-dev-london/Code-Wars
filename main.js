741fc6b479628d7e663fb92978bdbf5de20a48b7
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-06-03 15:52:07 +0100

    Convert string to an Array

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 console.clear();
 
-// Exes and Ohs
-
-let str = 'zzoo';
-console.log(XO(str));
-
-function XO(str) {
-  let x = str.match(/x/gi);
-  let o = str.match(/o/gi);
-  return (x && x.length) === (o && o.length);
-}
+const stringToArray = (str) => {
+  return str.split(' ');
+};
+console.log(stringToArray('John Smith'));
