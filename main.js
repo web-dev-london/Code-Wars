a4209aaa205c5cd40e5ee2993c6fb87f92295a3e
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-06-04 11:22:01 +0100

    Categorize New Member

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 console.clear();
 
-const removeSmallest = (numbers) => {
-  const copy = numbers.slice(0);
-  let smallestValue = numbers.indexOf(
-    Math.min(...numbers),
-  );
-  copy.splice(smallestValue, 1);
-  return copy;
+const openOrSenior = (date) => {
+  function membership(member) {
+    return member[0] >= 55 && member[1] > 7
+      ? 'Senior'
+      : 'Open';
+  }
+  return date.map(membership);
 };
 
-console.log(removeSmallest([2, 2, 1, 2, 1]));
+console.log(openOrSenior([78, 12]));
